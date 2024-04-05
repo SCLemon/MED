02ded52cc55f78fc012885cb5075753fc1418a97
SC1314520 <g0710126@hchs.hc.edu.tw>
2024-04-05 14:18:40 +0800

    config -> build 路徑調整

--- med/vue.config.js
+++ med/vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: "./",
+  outputDir: "dist",
+  assetsDir:"static",
   transpileDependencies: true,
   lintOnSave: false,
   devServer: {
