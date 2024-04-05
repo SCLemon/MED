99027ed4e30aa3391c300085fa382c49f7d4a40b
SC1314520 <g0710126@hchs.hc.edu.tw>
2024-04-05 13:39:04 +0800

    close the sw

--- med/src/main.js
+++ med/src/main.js
@@ -29,14 +29,14 @@ new Vue({
 console.warn = function() {};
 // console.error = function() {};
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./registerServiceWorker.js')
-      .then(registration => {
-        console.log('Service Worker registered:', registration);
-      })
-      .catch(error => {
-        console.error('Service Worker registration failed:', error);
-      });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('./registerServiceWorker.js')
+//       .then(registration => {
+//         console.log('Service Worker registered:', registration);
+//       })
+//       .catch(error => {
+//         console.error('Service Worker registration failed:', error);
+//       });
+//   });
+// }
