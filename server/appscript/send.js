7d484ca327a36a3ee3a903dbc69d34abc3e8c2a4
SC1314520 <g0710126@hchs.hc.edu.tw>
2024-04-30 21:48:02 +0800

    新增定時器自動寄送通知信

--- server/appscript/send.js
+++ server/appscript/send.js
@@ -1,17 +1,24 @@
+const schedule = require('node-schedule');
 const taskModel = require('../models/taskModel');
 const axios =require('axios');
 const { connectToDatabase } = require('../db/db');
 connectToDatabase();
 
-taskModel.find()
-.then((res,err)=>{
-    for(var i=0;i<res.length;i++){
-        res[i].data = res[i].data.filter(obj=>{
-            return new Date(obj.date).getDate() == new Date().getDate();
-        })
-    }
-    send(res);
-})
+const job = schedule.scheduleJob('*/1 * * * *', function() {
+    console.log('正常運行中，目前時間為：', new Date());
+});
+const dailyJob = schedule.scheduleJob('0 8 * * *', function() {
+    taskModel.find()
+    .then((res,err)=>{
+        for(var i=0;i<res.length;i++){
+            res[i].data = res[i].data.filter(obj=>{
+                return new Date(obj.date).getDate() == new Date().getDate();
+            })
+        }
+        send(res);
+    })
+});
+
 function send(data){
     axios.post('https://script.google.com/macros/s/AKfycbwL_FzBjtVrK4hoivhXjtWKaY6FLwhXofQxsJZw-IoCy0H4tRdhqPAxihIXlyvk5DUR/exec',{
         contents:data
