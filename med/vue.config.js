ec2464435d56f973b4333133f4ccd16c188bba33
SC1314520 <g0710126@hchs.hc.edu.tw>
2024-05-04 21:19:42 +0800

    加密混淆

--- med/vue.config.js
+++ med/vue.config.js
@@ -1,6 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
+const WebpackObfuscator = require('webpack-obfuscator');
+const TerserPlugin = require('terser-webpack-plugin');
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath:'./'
+  publicPath: './',
+  configureWebpack:(config)=>{
+    // config.optimization.minimizer[0].options.terserOptions.compress = {
+    //   drop_console: true,
+    // };
+    config.plugins.push(
+      new WebpackObfuscator({
+        rotateUnicodeArray: true,
+      })
+    );
+    config.optimization.minimizer.push(
+      new TerserPlugin({
+        terserOptions: {
+          mangle: true, // 开启变量名混淆
+        },
+      })
+    );
+  }
 })
