8d28887b4aba947aeddf09aa573b084efed96362
SC1314520 <g0710126@hchs.hc.edu.tw>
2024-05-05 23:41:38 +0800

    更改回 proxy 狀態

--- med/src/serverPath.js
+++ med/src/serverPath.js
@@ -1,5 +1,5 @@
-const host = 'http://192.168.100.136:3007'
-//const host = ''
+//const host = 'http://192.168.100.136:3007'  // http or non-proxy
+const host = ''    // proxy
 export {
     host
 }
--- med/vue.config.js
+++ med/vue.config.js
@@ -5,6 +5,35 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   publicPath: './',
+  devServer:{
+    https:true,
+    proxy: {
+      '/chat': {
+        target: 'http://127.0.0.1:3007',
+      },
+      '/verify': {
+        target: 'http://127.0.0.1:3007',
+      },
+      '/userInfo': {
+        target: 'http://127.0.0.1:3007',
+      },
+      '/reminder': {
+        target: 'http://127.0.0.1:3007',
+      },
+      '/filter': {
+        target: 'http://127.0.0.1:3007',
+      },
+      '/aiSetting': {
+        target: 'http://127.0.0.1:3007',
+      },
+      '/schedule': {
+        target: 'http://127.0.0.1:3007',
+      },
+      '/imageHistory':{
+        target: 'http://127.0.0.1:3007',
+      }
+    }
+  },
   configureWebpack:(config)=>{
     if (process.env.NODE_ENV === 'production') {
       config.plugins.push(
