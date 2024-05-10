7905a573a7103631f15aced5fd6cef369b7a9179
SC1314520 <g0710126@hchs.hc.edu.tw>
2024-05-10 22:19:05 +0800

    新增 API 註解

--- med/src/apiKey.js
+++ med/src/apiKey.js
@@ -1,5 +1,5 @@
-const openaiKey='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'
-const googleMapKey='AIzaSyDzUXKKyo-Cs3FNNRONt75fUrQEvUTraTE'
+const openaiKey='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'  // https://platform.openai.com/
+const googleMapKey='AIzaSyDzUXKKyo-Cs3FNNRONt75fUrQEvUTraTE' // https://cloud.google.com/apis?hl=zh-TW
 export {
     openaiKey,googleMapKey
 }
