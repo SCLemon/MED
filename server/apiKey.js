da926376234dc9999d0dce9c4cb199b2edfc6c3c
SC1314520 <g0710126@hchs.hc.edu.tw>
2024-10-16 22:50:08 +0800

    apiKey

--- server/apiKey.js
+++ server/apiKey.js
@@ -1,4 +1,8 @@
-
+const FugleAPIKey = '' // https://developer.fugle.tw/
+const NewsAPIKey = '' // https://newsapi.org/s/taiwan-news-api
+const googleMapKey='' // https://cloud.google.com/apis?hl=zh-TW
+const weatherKey = '' // https://www.visualcrossing.com/weather-api
+const openaiKey=''  // https://platform.openai.com/
 module.exports = {
     openaiKey,FugleAPIKey,NewsAPIKey,googleMapKey,weatherKey
 }
