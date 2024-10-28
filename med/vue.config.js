b3fec2fb85dea05d808d6a5878ffda79f1f2b5db
SC1314520 <g0710126@hchs.hc.edu.tw>
2024-10-28 16:26:21 +0800

    利用 cache 優化移動端的渲染速率

--- med/vue.config.js
+++ med/vue.config.js
@@ -1,12 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 const WebpackObfuscator = require('webpack-obfuscator');
 const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   publicPath: './',
-  devServer:{
-    https:true,
+  devServer: {
+    https: true,
     proxy: {
       '/chat': {
         target: 'http://127.0.0.1:3007',
@@ -29,34 +30,65 @@ module.exports = defineConfig({
       '/schedule': {
         target: 'http://127.0.0.1:3007',
       },
-      '/imageHistory':{
+      '/imageHistory': {
         target: 'http://127.0.0.1:3007',
       },
-      '/stock':{
+      '/stock': {
         target: 'http://127.0.0.1:3007',
       },
-      '/word':{
+      '/word': {
         target: 'http://127.0.0.1:3007',
       },
-      '/api':{
+      '/api': {
         target: 'http://127.0.0.1:3007',
       }
     }
   },
-  configureWebpack:(config)=>{
+  productionSourceMap: false,
+  pwa: {
+    name: 'MED Reminder',
+    short_name: 'MED Reminder',
+    themeColor: '#4DBA87',
+    backgroundColor: '#ffffff',
+    display: 'standalone',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/favicon-32x32.png',
+      maskIcon: 'img/icons/favicon-32x32.png',
+      msTileImage: 'img/icons/favicon-32x32.png',
+    },
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true,
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('.*\\.(js|css|html)$'), // 匹配所有的 JS、CSS 和 HTML 文件
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'assets-cache-v2.5.4',
+          },
+        },
+      ],
+    },
+  },
+  configureWebpack: (config) => {
     if (process.env.NODE_ENV === 'production') {
+      // 添加加密和混淆插件
       config.plugins.push(
         new WebpackObfuscator({
           rotateUnicodeArray: true,
         })
       );
+
+      // 使用 TerserPlugin 進行壓縮
       config.optimization.minimizer.push(
         new TerserPlugin({
           terserOptions: {
-            mangle: true,
+            extractComments: false,
           },
         })
       );
     }
   }
-})
+});
